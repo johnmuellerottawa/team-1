b1cc516bf03c17406c75aad236fdacd599021252
Administrator1 <Administrator1@sxh759-windows.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2019-04-03 14:03:45 +0000

    Created Exit command, added exit command to command factory and a Junit test.

---
+++ dos-box/test/command/library/CmdExitTest.java
@@ -0,0 +1,25 @@
+/*
+ * DOSBox, Scrum.org, Professional Scrum Developer Training
+ * Authors: Rainer Grau, Daniel Tobler, Zuehlke Technology Group
+ * Copyright (c) 2013 All Right Reserved
+ */ 
+
+package command.library;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CmdExitTest extends CmdTest {
+
+    @Before
+    public void setUp()
+    {
+    	// do nothing by intention
+    }
+
+    @Test
+    public void cmdExit()
+    {
+    	// do nothing by intention
+    }
+}
